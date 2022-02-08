1df7eb5c77e897c5fac4c94ec7e1ddad12386c68
Mahmoud Abdelbary <mahmoud.abdelbaty@gmail.com>
2022-02-08 23:29:46 +0200

    Time: 2292 ms (5.01%), Space: 53.7 MB (44.54%) - LeetHub

--- 127-word-ladder/127-word-ladder.java
+++ 127-word-ladder/127-word-ladder.java
@@ -1,64 +1,98 @@
 class Solution {
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Set<String> wordSet = new HashSet<String>();
-        Set<String> seenWords = new HashSet<>();
-        
-        for(String word : wordList)
+    int characterDistance(String firstWord, String secondWord)
+    {
+        int dis = 0;
+        for(int i = 0 ; i < firstWord.length() ; i++)
         {
-            wordSet.add(word);
-        }
-        
-        Deque<String> q = new ArrayDeque<String>();
-        
-        q.offerLast(beginWord);
-        
-        int dis = 1; 
-        int lvlCount = 1;
-        int nextLvlCount  = 0;
-        
-        while(q.isEmpty() == false)
-        {
-            String currWord = q.removeFirst();
-            
-            if(currWord.equals(endWord))
+            if(firstWord.charAt(i) != secondWord.charAt(i))
             {
-                return dis;
-            }
-            
-            lvlCount--;
-            char[] tempWord = currWord.toCharArray();
-            
-            for(int i =  0 ; i < currWord.length();i++)
-            {
-               for(char c = 'a' ; c <= 'z' ; c++)
-               {
-                   if(c == tempWord[i])
-                   {
-                       continue;
-                   }
-                   
-                   tempWord[i] = c;
-                   if(seenWords.contains(String.valueOf(tempWord)) == false && wordSet.contains(String.valueOf(tempWord)))
-                   {
-                       seenWords.add(String.valueOf(tempWord));
-                       q.offerLast(String.valueOf(tempWord));
-                       nextLvlCount++;
-                   }
-                   tempWord[i] = currWord.charAt(i);
-               }
-                
-            }
-            
-            if(lvlCount == 0)
-            {
-                lvlCount = nextLvlCount;
-                nextLvlCount = 0;
                 dis++;
             }
         }
         
-        return 0;
-        
-        
+        return dis;
     }
+    
+    
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        if(wordList.contains(endWord) == false)
+        {
+            return 0;
+        }
+        
+        Map<String,HashSet<String>> adjList= new HashMap<String,HashSet<String>>();
+        
+        HashSet<String> adj = new HashSet<String>();
+        
+        for(String word : wordList)
+        {
+            if(characterDistance(beginWord,word) == 1)
+            {
+                adj.add(word);
+            }
+        }
+        
+        adjList.put(beginWord,adj);
+        
+        for(String word : wordList)
+        {
+            adj = new HashSet<String>();
+            for(String otherWord : wordList)
+            {
+                if(characterDistance(word,otherWord) == 1)
+                {
+                    adj.add(otherWord);
+                }
+            }
+            adjList.put(word,adj);
+        }
+        
+        return bfs(adjList,beginWord,endWord);    
+    }
+    
+    int bfs(Map<String,HashSet<String>> adjList, String beginWord, String endWord)
+    {
+        Deque<String> queue = new ArrayDeque<String>();
+        Set<String> visited = new HashSet<String>();
+        
+        queue.offerLast(beginWord);
+        visited.add(beginWord);
+        
+        int currentLevelNodeCount = 1;
+        int nextLevelNodeCount = 0;
+        int level = 1;
+        
+        while(queue.isEmpty() == false)
+        {
+            String currentWord = queue.removeFirst();
+            
+            if(endWord.compareTo(currentWord) == 0)
+            {
+                return level;
+            }
+            
+            currentLevelNodeCount--;
+            
+            for(String word : adjList.get(currentWord))
+            {
+                if(visited.contains(word) == false)
+                {
+                    queue.offerLast(word);
+                    visited.add(word);
+                    nextLevelNodeCount++;
+                }
+            }
+            
+            if(currentLevelNodeCount == 0)
+            {
+                currentLevelNodeCount = nextLevelNodeCount;
+                nextLevelNodeCount = 0 ;
+                level++;
+            }
+            
+        }
+        
+        return 0;
+    }
+    
 }
