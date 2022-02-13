f0dcf60e15ca09f2dae2a4d1f4e11a8d81607966
Mahmoud Abdelbary <mahmoud.abdelbaty@gmail.com>
2022-02-13 14:27:32 +0200

    Time: 27 ms (26.78%), Space: 43.4 MB (7.00%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.java
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.java
@@ -1,51 +1,85 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 class Solution {
-    List<Integer> ans;
-    TreeNode target;
-    int K;
-    public List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
-        ans = new LinkedList();
-        this.target = target;
-        this.K = K;
-        dfs(root);
-        return ans;
-    }
-
-    // Return vertex distance from node to target if exists, else -1
-    // Vertex distance: the number of vertices on the path from node to target
-    public int dfs(TreeNode node) {
-        if (node == null)
-            return -1;
-        else if (node == target) {
-            subtree_add(node, 0);
-            return 1;
-        } else {
-            int L = dfs(node.left), R = dfs(node.right);
-            if (L != -1) {
-                if (L == K) ans.add(node.val);
-				else{
-                subtree_add(node.right, L + 1);
-				}
-                return L + 1;
-            } else if (R != -1) {
-                if (R == K) ans.add(node.val);
-				else{
-                subtree_add(node.left, R + 1);
-				}
-                return R + 1;
-            } else {
-                return -1;
+    HashMap<TreeNode,TreeNode> nodeParent = new HashMap<TreeNode,TreeNode>();
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        
+        List<Integer> distanteKNodes = new ArrayList<Integer>();
+        
+        dfs(root,null);
+        
+        Deque<TreeNode> queue = new ArrayDeque<TreeNode>();
+        HashSet<TreeNode> visitedNodes = new HashSet<TreeNode>();
+        
+        queue.offerLast(target);
+        
+        visitedNodes.add(target);
+        int currentLevelNodes = 1;
+        int nextLevelNodes    = 0;
+        int level = 0;
+        
+        while(queue.isEmpty() == false)
+        {
+            if(level == k)
+            {
+                //System.out.print("size " + queue.size() );
+                for(int i = 0 ; i < currentLevelNodes ; i++)
+                {
+                    distanteKNodes.add(queue.removeFirst().val);   
+                }
+                break;
+            }
+            TreeNode currentNode = queue.removeFirst();
+            currentLevelNodes--;
+            //System.out.println(currentNode.val + " " +  level);
+            if(currentNode.left != null && visitedNodes.contains(currentNode.left) == false)
+            {
+                queue.offerLast(currentNode.left);
+                visitedNodes.add(currentNode.left);
+                nextLevelNodes++;
+            }
+            if(currentNode.right != null && visitedNodes.contains(currentNode.right) == false)
+            {
+                queue.offerLast(currentNode.right);
+                visitedNodes.add(currentNode.right);
+                nextLevelNodes++;
+            }
+            if(nodeParent.get(currentNode) != null && visitedNodes.contains(nodeParent.get(currentNode)) == false)
+            {
+                queue.offerLast(nodeParent.get(currentNode));
+                visitedNodes.add(nodeParent.get(currentNode));
+                nextLevelNodes++;
+            }
+            
+            if(currentLevelNodes == 0)
+            {
+                level++;
+                currentLevelNodes = nextLevelNodes;
+                nextLevelNodes = 0;
             }
         }
+        
+        return distanteKNodes;
     }
-
-    // Add all nodes 'K - dist' from the node to answer.
-    public void subtree_add(TreeNode node, int dist) {
-        if (node == null) return;
-        if (dist == K)
-            ans.add(node.val);
-        else {
-            subtree_add(node.left, dist + 1);
-            subtree_add(node.right, dist + 1);
+    
+    void dfs(TreeNode node, TreeNode parent)
+    {
+        if(node == null)
+        {
+            return;
         }
+        nodeParent.put(node,parent);
+        
+        dfs(node.right,node);
+        dfs(node.left,node);
+        
+        return;
     }
 }
