b0f09cb287994d88afb802878fd87fda12ea49a7
Mahmoud Abdelbary <mahmoud.abdelbaty@gmail.com>
2022-02-19 17:32:01 +0200

    Time: 2 ms (8.56%), Space: 43.6 MB (21.19%) - LeetHub

--- 112-path-sum/112-path-sum.java
+++ 112-path-sum/112-path-sum.java
@@ -19,11 +19,45 @@ class Solution {
         {
             return false;
         }
-        if(root.right == null && root.left == null && root.val == targetSum)
+        Deque<TreeNode> nodeStack = new ArrayDeque<TreeNode>();
+        Deque<Integer> sumStack = new ArrayDeque<Integer>();
+        
+        nodeStack.offerLast(root);
+        sumStack.offerLast(targetSum);
+        
+        while(nodeStack.isEmpty() == false)
+        {
+            TreeNode currentNode = nodeStack.removeLast();
+            int currentSum = sumStack.removeLast() - currentNode.val;
+            
+            if(currentNode.left == null && currentNode.right == null && currentSum == 0)
+            {
+                return true;
+            }
+            
+            if(currentNode.left != null)
+            {
+                nodeStack.offerLast(currentNode.left);
+                sumStack.offerLast(currentSum);
+            }
+            if(currentNode.right != null)
+            {
+                nodeStack.offerLast(currentNode.right);
+                sumStack.offerLast(currentSum);
+            }
+        }
+        return false;
+        /*
+        if(root == null)
+        {
+            return false;
+        }
+        if(root.right == null root.val == targetSum)
         {
             return true;
         }
         
         return hasPathSum(root.right,targetSum-root.val) || hasPathSum(root.left,targetSum-root.val);
+        */
     }
 }
