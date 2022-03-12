2b1c547bdbad9c784eea635624122e298ff293e9
Mahmoud Abdelbary <mahmoud.abdelbaty@gmail.com>
2022-03-12 16:19:55 +0200

    Time: 0 ms (100.00%), Space: 47.1 MB (40.29%) - LeetHub

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -0,0 +1,76 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] result = new int[] { -1, -1 };
+        if(nums.length == 0)
+        {
+            return result;
+        }
+
+
+        result[0] = lowerBound(nums,target);
+        result[1] = upperBound(nums,target);
+
+        return result;
+  }
+
+  static int lowerBound(int[] arr,int key)
+  {
+    if(key > arr[arr.length-1])
+    {
+      return -1;
+    }
+    int start = 0 ;
+    int end = arr.length-1;
+    int mid;
+
+    while(start < end)
+    {
+      mid = start + (end - start)/2;
+
+      if(arr[mid] < key)
+      {
+        start = mid+1;
+      }
+      else
+      {
+        end = mid;
+      }
+    }
+    if(arr[start] != key)
+    {
+      return -1;
+    }
+    return start;
+  }
+
+  static int upperBound(int[] arr,int key)
+  {
+    if(key > arr[arr.length-1])
+    {
+      return -1;
+    }
+    int start = 0 ;
+    int end = arr.length-1;
+    int mid;
+
+    while(start < end)
+    {
+      mid = start + (end -start+1)/2;
+
+      if(arr[mid] <= key)
+      {
+        start = mid;
+      }
+      else
+      {
+        end = mid-1;
+      }
+    }
+    if(arr[start] != key)
+    {
+      return -1;
+    }
+    return start;
+  }
+    
+}
