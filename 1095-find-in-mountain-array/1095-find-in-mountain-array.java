8ffca014b3b9e7584f5576d5eb7af2497e1fca1b
Mahmoud Abdelbary <mahmoud.abdelbaty@gmail.com>
2022-03-13 15:33:53 +0200

    Time: 1 ms (29.36%), Space: 41.9 MB (63.42%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.java
@@ -0,0 +1,90 @@
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index) {}
+ *     public int length() {}
+ * }
+ */
+ 
+class Solution {
+    int findPeakIndex(MountainArray mountainArr)
+    {
+        int start = 0 ;
+        int end = mountainArr.length()-1;
+        int mid;
+        
+        while(start < end)
+        {
+            mid = start + (end-start)/2;
+            
+            if(mid < mountainArr.length()-1 && mountainArr.get(mid) < mountainArr.get(mid + 1))
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid;
+            }
+        }
+        
+        return start;
+    }
+    int binarySearch(MountainArray mountainArr,int start,int end,int target)
+    {
+        int mid;
+        boolean asending = (mountainArr.get(start) <= mountainArr.get(end));
+        
+        if(asending)
+        {
+            while(start < end)
+            {
+                mid = start + (end - start)/2;
+
+                if(mountainArr.get(mid) < target)
+                {
+
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid;
+                }
+            }
+        }else
+        {
+           while(start < end)
+            {
+                mid = start + (end - start + 1)/2;
+
+                if(mountainArr.get(mid) >= target)
+                {
+                    start = mid;
+                }
+                else
+                {
+                    end = mid-1;
+                }
+            } 
+        }
+        
+        if(mountainArr.get(start) != target)
+        {
+            
+            System.out.println(mountainArr.get(start));
+            return -1;
+        }
+        return start;
+    }
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        
+        int peek = findPeakIndex(mountainArr);
+        int leftPos = binarySearch(mountainArr,0,peek-1,target);
+        if( leftPos == -1)
+        {
+            leftPos = binarySearch(mountainArr,peek,mountainArr.length()-1,target);
+        }
+        
+        return leftPos;
+    }
+}
