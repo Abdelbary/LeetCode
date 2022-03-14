f31c97880d0afeeaf88e11818ddcc2f3e66fe905
Mahmoud Abdelbary <mahmoud.abdelbaty@gmail.com>
2022-03-14 12:13:24 +0200

    Time: 4 ms (65.31%), Space: 127.8 MB (33.49%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -8,11 +8,7 @@ class Solution {
                 swap(nums,i,nums[i]-1);
             }
         }
-        for(int i = 0 ; i < nums.length ; i++)
-        {
-            System.out.print(nums[i] + " ");
-        }
-        System.out.println();
+    
         for(int i = 0 ; i < nums.length ; i++)
         {
             
