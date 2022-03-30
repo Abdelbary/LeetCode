1fb13eab7e2ed846c9b537e488cc7d6f16e88d62
Mahmoud Abdelbary <mahmoud.abdelbaty@gmail.com>
2022-03-30 23:10:38 +0200

    Time: 0 ms (100.00%), Space: 41.4 MB (18.85%) - LeetHub

--- 476-number-complement/476-number-complement.java
+++ 476-number-complement/476-number-complement.java
@@ -1,13 +1,5 @@
 class Solution {
     public int findComplement(int num) {
-        int todo = num , bit = 1;
-        
-        while(todo != 0)
-        {
-            todo >>=1;
-            num = num ^ bit;
-            bit <<=1;
-        }
-        return num;
+        return ~num & (Integer.highestOneBit(num) - 1);
     }
 }
