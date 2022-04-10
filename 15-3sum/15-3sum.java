daf6bc784849a66665ea64e534c17b4b7382e428
Mahmoud Abdelbary <mahmoud.abdelbaty@gmail.com>
2022-04-10 23:27:27 +0200

    Time: 29 ms (65.64%), Space: 58.8 MB (60.05%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -6,30 +6,29 @@ class Solution{
         
         for(int i = 0 ; i < nums.length -2; i++)
         {
-            while(i > 0 && i < nums.length && nums[i] == nums[i-1]) i++;
+            if(i > 0 && nums[i] == nums[i-1]) continue; //Skip duplicates
             
-            int left = i + 1;
-            int right = nums.length-1;
-            
-            while(i < nums.length-2 && left < right)
-            {
-                int sum = nums[left] + nums[right] + nums[i];
-                if(sum == 0)
-                {
-                    allPairs.add(List.of(nums[i],nums[left++],nums[right--]));
-                    while(left < right && nums[left] == nums[left-1])   left++;
-                }
-                else if(sum >  0)
-                {
-                        right--;
-                }
-                else
-                {
-                    left++;
-                }
-            }
+            twoSums(i,nums,allPairs);
         }
         
         return allPairs;
+    }
+    
+    void twoSums(int start, int[] nums, List<List<Integer>> ans) {
+        int left = start+1;
+        int right = nums.length - 1;
+        
+        while(left < right)
+        {
+            int sum = nums[start] + nums[left] + nums[right];
+            
+            if(sum == 0)
+            {
+                ans.add(List.of(nums[start],nums[left++],nums[right--]));
+                while(left < right && nums[left] == nums[left-1])left++;
+            }
+            else if(sum > 0)    right--;
+            else                left++;
+        }
     }
 }
