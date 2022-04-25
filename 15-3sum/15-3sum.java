18dbc3184613ea25ac2c1873ebae6379f6da65a6
Mahmoud Abdelbary <mahmoud.abdelbaty@gmail.com>
2022-04-25 21:04:19 +0200

    Time: 30 ms (59.93%), Space: 58.8 MB (53.69%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -2,35 +2,38 @@ class Solution{
    
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> allPairs = new ArrayList<List<Integer>>();
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
         
-        for(int i = 0 ; i < nums.length -2; i++)
+        for(int i = 0 ; i < nums.length ; i++)
         {
-            if(i > 0 && nums[i] == nums[i-1]) continue; //Skip duplicates
-            
-            twoSums(i,nums,allPairs);
+            if(i == 0 || nums[i] != nums[i-1])
+                ans.addAll(twoSum(nums,i+1,nums.length-1,nums[i]));
         }
         
-        return allPairs;
+        return ans;
     }
     
-    void twoSums(int start, int[] nums, List<List<Integer>> ans) {
-        int left = start+1;
-        int right = nums.length - 1;
+    public List<List<Integer>> twoSum(int[] nums,int left,int right,int target)
+    {
+        List<List<Integer>> triples = new ArrayList<List<Integer>>();
         
         while(left < right)
         {
-            int sum = nums[start] + nums[left] + nums[right];
+            int sum = nums[left] + nums[right] + target;
             
             if(sum == 0)
             {
-                ans.add(List.of(nums[start],nums[left++],nums[right--]));
-                while(left < right && nums[left] == nums[left-1])left++;
-                while(left < right && nums[right] == nums[right+1])right--;
-
+                triples.add(List.of(target,nums[left],nums[right]));
+                left++;
+                right--;
+                while(left < right && nums[left] == nums[left-1]) left++;// to remove duplicates
             }
-            else if(sum > 0)    right--;
-            else                left++;
+            else if(sum > 0)
+                right--;
+            else
+                left++;
         }
+        
+        return triples;
     }
 }
