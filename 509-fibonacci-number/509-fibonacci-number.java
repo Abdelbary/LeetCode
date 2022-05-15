d5d134f7edec4ff7e1d031ac5a5538b62cc142b1
Mahmoud Abdelbary <mahmoud.abdelbaty@gmail.com>
2022-05-15 17:51:39 +0200

    Time: 0 ms (100.00%), Space: 41 MB (40.84%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -2,6 +2,16 @@ class Solution {
     public int fib(int n) {
         if(n <= 1) return n;
         
-        return fib(n-1) + fib(n-2);
+        int fibt_1 = 0;
+        int fibt_2 = 1;
+        int ans = 0;
+        for(int i = 2 ; i <= n ; i++)
+        {
+            ans = fibt_1 + fibt_2;
+            fibt_1 = fibt_2;
+            fibt_2 = ans;
+        }
+        
+        return ans;
     }
 }
