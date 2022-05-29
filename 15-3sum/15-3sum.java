22f2c433d419db4558512aac285e1e9c5e8996b0
Mahmoud Abdelbary <mahmoud.abdelbaty@gmail.com>
2022-05-29 12:49:39 +0200

    Time: 28 ms (68.60%), Space: 46.4 MB (91.77%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,39 +1,43 @@
 class Solution{
-   
-    public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
+    private List<List<Integer>> twoSum(int target,int start,int[] nums)
+    {
+        int left  = start;
+        int right = nums.length-1;
         List<List<Integer>> ans = new ArrayList<List<Integer>>();
         
-        for(int i = 0 ; i < nums.length ; i++)
-        {
-            if(i == 0 || nums[i] != nums[i-1])
-                ans.addAll(twoSum(nums,i+1,nums.length-1,nums[i]));
-        }
-        
-        return ans;
-    }
-    
-    public List<List<Integer>> twoSum(int[] nums,int left,int right,int target)
-    {
-        List<List<Integer>> triples = new ArrayList<List<Integer>>();
         
         while(left < right)
         {
-            int sum = nums[left] + nums[right] + target;
-            
-            if(sum == 0)
+            if(nums[left] + nums[right]  == -target )
             {
-                triples.add(List.of(target,nums[left],nums[right]));
+                ans.add(List.of(target,nums[left],nums[right]));
                 left++;
                 right--;
-                while(left < right && nums[left] == nums[left-1]) left++;// to remove duplicates
+                        
+                while(left < nums.length && nums[left] == nums[left-1]) left++; //remove duplicates
             }
-            else if(sum > 0)
+            else if(nums[left]+nums[right] > -target)
                 right--;
             else
                 left++;
+        }
+
+        return ans;
+    }
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> triples = new ArrayList<List<Integer>>();
+        
+        for(int i = 0 ; i < nums.length-2 ; i++)
+        {
+            if(i == 0 || nums[i] != nums[i-1])
+            {
+                triples.addAll(twoSum(nums[i],i+1,nums));      
+            }
         }
         
         return triples;
     }
 }
+    
+   
