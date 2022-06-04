5f0858798e6fcafb3afced2f88448b2692d96329
Mahmoud Abdelbary <mahmoud.abdelbaty@gmail.com>
2022-06-04 19:31:51 +0200

    Time: 24 ms (58.20%), Space: 43.2 MB (75.23%) - LeetHub

--- 18-4sum/18-4sum.java
+++ 18-4sum/18-4sum.java
@@ -1,61 +1,56 @@
 class Solution {
-    public List<List<Integer>> fourSum(int[] nums, int target) {
-        Arrays.sort(nums);
-        
-        return recursiveFourSum(nums,0,target,4); 
-    }
-    
-    public List<List<Integer>> recursiveFourSum(int[] nums,int start,int target, int k)
-    {
-        
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
-        
-        if(k == 2)
-            return twoSum(nums,start,target);
-        
-        if(start == nums.length)
-            return result;
-        
-        //optimization
-        int average = target/k;
-        
-        if(average < nums[start] || average > nums[nums.length-1])
-            return result;
-        
-        for(int i = start ; i < nums.length ; i++)
-        {
-            if(i == start || nums[i] != nums[i-1])
-            {
-                for(List<Integer> subList : recursiveFourSum(nums,i+1,target - nums[i],k-1))
-                {
-                    result.add(new ArrayList<>(Arrays.asList(nums[i])));
-                    result.get(result.size()-1).addAll(subList);
-                }
-            }
-        }
-        
-        return result;
-    }
-    
-    public List<List<Integer>> twoSum(int[] nums,int start,int target)
-    {
+     private  List<List<Integer>> twoSum(int[] arr,int target,int start)
+      {
         int left = start;
-        int right = nums.length-1;
+        int right = arr.length-1;
         List<List<Integer>> result = new ArrayList<List<Integer>>();
-        
+
         while(left < right)
         {
-            int sum = nums[left] + nums[right];
-          
-            if(sum < target || (left > start && nums[left] == nums[left-1]))
-                left++;
-            else if(sum > target || (right < nums.length-1 && nums[right] == nums[right+1])) 
-                right--;
-            else
-                result.add(Arrays.asList(nums[left++],nums[right--]));
+          int sum = arr[left] + arr[right];
+
+          if(sum == target)
+          {
+            result.add(new ArrayList<Integer>(Arrays.asList(arr[left],arr[right]))); 
+            left++;
+            right--;   
+            while(left < arr.length && arr[left] == arr[left-1]) left++; // to remove duplicates
+          }
+          else if(sum > target)
+            right--;
+          else 
+            left++;
         }
         return result;
+      }
+
+      private  List<List<Integer>> helper(int[] arr,int target,int level,int start)
+      {
+        if(level == 2)
+          return twoSum(arr,target,start);
+
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+        for(int i = start ; i <= arr.length - level ; i++)
+        {
+          if(i == start || arr[i] != arr[i-1])
+          {
+            List<List<Integer>> tmp =  helper(arr,target-arr[i],level-1,i+1);
+
+            for(List<Integer> list : tmp)
+              list.add(arr[i]);
+
+            result.addAll(tmp);
+          }
+        }
+
+        return result;
+      }
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> quadruplets = new ArrayList<>();
+        // TODO: Write your code here
+        quadruplets.addAll(helper(nums,target,4,0));
+        return quadruplets;
     }
 }
-    
-    
