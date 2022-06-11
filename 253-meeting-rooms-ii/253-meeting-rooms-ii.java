2cf48976ead1bd5a5d07096a7cd81ce20bf2f36b
Mahmoud Abdelbary <mahmoud.abdelbaty@gmail.com>
2022-06-11 19:22:45 +0200

    Time: 4 ms (94.60%), Space: 45.7 MB (19.69%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.java
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.java
@@ -1,6 +1,7 @@
 class Solution {
-    public int minMeetingRooms(int[][] intervals) {
-       Arrays.sort(intervals,(a,b)->(a[0]-b[0]));
+    private int firstApproach(int[][] intervals)
+    {
+        Arrays.sort(intervals,(a,b)->(a[0]-b[0]));
         
         PriorityQueue<int[]> minHeap = new PriorityQueue<int[]>((a,b)->(a[1]-b[1]));
         
@@ -18,5 +19,49 @@ class Solution {
         
         
         return minRooms;
+    }
+    
+    private int secondApproach(int[][] intervals)
+    {
+        int[] start = new int[intervals.length];
+        int[] end = new int[intervals.length];
+        
+        
+        for(int i = 0 ; i < intervals.length ; i++)
+        {
+            start[i] = intervals[i][0];
+            end[i] = intervals[i][1];
+        }
+        Arrays.sort(start);
+        Arrays.sort(end);
+        
+        int s = 0;
+        int e = 0;
+        
+        int meetingRooms = 0;
+        int minRooms = Integer.MIN_VALUE;
+        
+        while(s < start.length)
+        {
+            if(start[s] < end[e])
+            {
+                meetingRooms++;
+                s++;
+            }
+            else
+            {
+                meetingRooms--;
+                e++;
+            }
+            
+            minRooms = Math.max(meetingRooms,minRooms);
+        }
+        
+        return minRooms;
+    }
+    public int minMeetingRooms(int[][] intervals) {
+        
+        //return firstApproach(intervals);
+        return secondApproach(intervals);
     }
 }
