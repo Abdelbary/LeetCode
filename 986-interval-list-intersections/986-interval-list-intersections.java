fefaadb1693e22e5c90c816a94c57cf87c2585a4
Mahmoud Abdelbary <mahmoud.abdelbaty@gmail.com>
2022-06-11 15:08:19 +0200

    Time: 6 ms (29.80%), Space: 55 MB (24.60%) - LeetHub

--- 986-interval-list-intersections/986-interval-list-intersections.java
+++ 986-interval-list-intersections/986-interval-list-intersections.java
@@ -1,22 +1,32 @@
 class Solution {
     public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
+        /*
+            - 2-pointers approach one for each list
+            - if overlapping condition met [maxStart > minEnd]:
+                -add the overlapped part to answer
+            - move to next next valid state for overlapping [move minEnd pointer]
+        */
+        
+        
         int i = 0;
         int j = 0;
-        List<int[]> overlappingIntervals = new ArrayList<int[]>();
+        
+        List<int[]> overlapping = new ArrayList<int[]>();
+        
         while(i < firstList.length && j < secondList.length)
         {
             int maxStart = Math.max(firstList[i][0],secondList[j][0]);
-            int minEnd =  Math.min(firstList[i][1],secondList[j][1]);
-            if(maxStart <= minEnd )
+            int minEnd   = Math.min(firstList[i][1],secondList[j][1]);
+            
+            if(maxStart <= minEnd)//overlapping
             {
-                overlappingIntervals.add(new int[]{maxStart,minEnd});
-                
-               
+                overlapping.add(new int[]{maxStart,minEnd});        
             }
-             if(firstList[i][1] <= minEnd) i++;
-             if(secondList[j][1] <= minEnd) j++;
+            
+            i += (firstList[i][1] <= minEnd)? 1 : 0;
+            j += (secondList[j][1] <= minEnd)? 1 : 0;
         }
-                
-        return overlappingIntervals.toArray(new int[overlappingIntervals.size()][2]);
+        
+        return overlapping.toArray(new int[overlapping.size()][2]);
     }
 }
