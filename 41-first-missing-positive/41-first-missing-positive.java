50d3ce7dd866b71187cd285347d14a76f1a6c731
Mahmoud Abdelbary <mahmoud.abdelbaty@gmail.com>
2022-06-12 19:18:15 +0200

    Time: 5 ms (48.89%), Space: 122 MB (71.55%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -4,27 +4,22 @@ class Solution {
         for(int i = 0 ; i < nums.length ; i++)
         {
             while(nums[i]-1 >= 0 && nums[i]-1 < nums.length && nums[i] != nums[nums[i]-1])
-            {
-                swap(nums,i,nums[i]-1);
-            }
+                swap(i,nums[i]-1,nums);
         }
-    
-        for(int i = 0 ; i < nums.length ; i++)
-        {
-            
-            if(i != nums[i]-1)
-            {
+        
+        for(int i = 0 ; i < nums.length ;i++)
+            if(nums[i]-1 != i)
                 return i+1;
-            }
-        }
+        
         
         return nums.length+1;
     }
     
-    void swap(int[] nums,int i,int j)
+    
+    private void swap(int x,int y, int[] nums)
     {
-        int tmp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = tmp;
+        int tmp = nums[x];
+        nums[x] = nums[y];
+        nums[y] = tmp;
     }
 }
